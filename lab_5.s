c431a70dc1cb6d7d7a0af3e4dadbbff1bee37ad9
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-03-10 22:52:29 -0400

    Goes to both handlers

--- lab_5.s
+++ lab_5.s
@@ -1,14 +1,18 @@
 	.data
 
 	.global prompt
-	.global mydata
+	;.global mydata
+	.global buttonScore
+	.global spaceScore
+	.global roundState
 
 prompt:	.string "Your prompt with instructions is place here", 0
-mydata:	.byte	0x20	; This is where you can store data.
+;mydata:	.byte	0x20	; This is where you can store data.
 			; The .byte assembler directive stores a byte
 			; (initialized to 0x20) at the label mydata.
 			; Halfwords & Words can be stored using the
 			; directives .half & .word
+roundState: .byte 0
 
 	.text
 
@@ -25,17 +29,28 @@ mydata:	.byte	0x20	; This is where you can store data.
 	.global lab5
 
 ptr_to_prompt:		.word prompt
-ptr_to_mydata:		.word mydata
+;ptr_to_mydata:		.word mydata
+ptr_to_buttonScore: .word buttonScore
+ptr_to_spaceScore: .word spaceScore
+ptr_to_roundState: 	.word roundState
 
 lab5:								; This is your main routine which is called from
 ; your C wrapper.
 	PUSH {r4-r12,lr}   		; Preserve registers to adhere to the AAPCS
 	ldr r4, ptr_to_prompt
-	ldr r5, ptr_to_mydata
+	;ldr r5, ptr_to_mydata
 
  	bl uart_init
 	bl uart_interrupt_init
 	bl gpio_interrupt_init
+	NOP
+	NOP
+	NOP
+
+
+LOOP5:
+
+	B LOOP5
 
 	; This is where you should implement a loop, waiting for the user to
 	; enter a q, indicating they want to end the program.
